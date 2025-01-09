18f97d07e557f14a5ce26313fe2a95911cba9ad4
Alvaro Navarro <quinchonavarro@gmail.com>
2025-01-08 21:04:13 -0400

    Github logo

---
+++ src/screens/Home.jsx
@@ -0,0 +1,17 @@
+import Logo from "../Components/Home/Logo.jsx";
+import Github from "../Components/Home/Github.jsx";
+import Body from "../Components/Home/Body.jsx";
+import Footer from "../Components/Home/Footer.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <Logo />
+      <Github />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home;
