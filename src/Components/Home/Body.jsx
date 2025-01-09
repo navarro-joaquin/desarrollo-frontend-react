8b66b5747b116f1e0808d106e9d2af75c6186446
Alvaro Navarro <quinchonavarro@gmail.com>
2025-01-08 20:50:09 -0400

    Components part 1

---
+++ src/Components/Home/Body.jsx
@@ -0,0 +1,16 @@
+const Body = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Vite + React</h1>
+        <h2>Desarrollo Web Full Stack FrontEnd y BackEnd</h2>
+        <h2>Modulo VII</h2>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </div>
+  )
+};
+
+export default Body;
