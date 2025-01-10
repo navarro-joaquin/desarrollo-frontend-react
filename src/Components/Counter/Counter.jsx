ea6694ae4e886755e89bd4a4284cb4a80b5d5fa3
Alvaro Navarro <quinchonavarro@gmail.com>
2025-01-09 21:55:27 -0400

    reducer methods

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './../../store/features/counter/counterSlice.js'
+import { decrement, increment, incrementByAmount } from './../../store/features/counter/counterSlice.js'
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.value)
@@ -22,6 +22,11 @@ const Counter = () => {
           onClick={() => dispatch(decrement())}
         >
           Decrement
+        </button><button
+          aria-label="Decrement value"
+          onClick={() => dispatch(incrementByAmount(20))}
+        >
+          Decrement by Amount
         </button>
       </div>
     </div>
