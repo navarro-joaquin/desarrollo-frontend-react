dbe140a244bde51c3c25ab075cadcecc2ac01400
Alvaro Navarro <quinchonavarro@gmail.com>
2025-01-12 23:41:09 -0400

    Practica 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Home from "./screens/Home.jsx";
 import Counter from "./Components/Counter/Counter.jsx";
+import Product from "./screens/Product.jsx";
 
 function App() {
 
   return (
     <>
-      <Home />
-      <Counter />
+      {/*<Home />*/}
+      {/*<Counter />*/}
+      <Product />
     </>
   )
 }
